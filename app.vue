faff082689942e10c918b7501ae9f00102abd7e1
zeng <3117005065@mail2.gdut.edu.cn>
2021-12-03 18:15:14 +0800

    feat: 🍱 按需导入devUI组件库

--- app.vue
+++ app.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h3>hello nuxt3</h3>
+    <d-button>我是vueui的按钮</d-button>
     <!-- 路由出口 -->
     <NuxtPage></NuxtPage>
   </div>
 </template>
+
+<script lang="ts" setup>
+const { $hello } = useNuxtApp();
+console.log($hello());
+</script>
